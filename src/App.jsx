bb805a355efee1b4bd4afe0b30727ea022f88ec0
niya-s <niya.somkerd@gmail.com>
2025-01-21 15:42:17 +0700

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { Routes, Route } from "react-router"
+import { Link, Routes, Route } from "react-router"
 
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <nav className="flex justify-between align-middle h-8 bg-slate-300 items-center p-2">
+          <div className="flex gap-5">
+            <Link to='/'>Home</Link>
+            <Link to='about'>About</Link>
+            <Link to='contact'>Contact</Link>
+        </div>
+        
+        <div className="flex gap-5">
+          <Link to='login'>Log in</Link>
+          <Link to='register'>Register</Link>
+        </div>
+        
+      </nav>
+
     <Routes>
       <Route path="/" element={<a href='/about' style={{ color: 'blue' }}>Home</a>}/>
       <Route path="about" element={<a href='/' style={{ color: 'blue' }}>About</a>}/>
+      <Route path="contact" element={<a href='/' style={{ color: 'blue' }}>Contact</a>}/>
+      <Route path="login" element={<a href='/' style={{ color: 'blue' }}>Log in Page</a>}/>
+      <Route path="register" element={<a href='/' style={{ color: 'blue' }}>Register</a>}/>
+
+      <Route path="admin"> {/* parent */}
+        <Route path="dashboard" element={<a href='/' style={{ color: 'green' }}>Dashboard page</a>}/>
+        <Route path="manage" element={<a href='/' style={{ color: 'red' }}>Manage page</a>}/>
+        <Route path="setting" element={<a href='/' style={{ color: 'red' }}>Setting page</a>}/>
+      </Route>
+
     </Routes>
     </>
   )
